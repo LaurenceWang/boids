fba47fa27a8e4d9118d438e298a010cb0bfc3bd0
Elia <eliaroard@gmail.com>
2023-04-12 14:47:06 +0200

    Jules' review

--- src/Fish.cpp
+++ src/Fish.cpp
@@ -124,16 +124,18 @@ void Fish::applyObstacleForces(std::vector<Obstacle> const& obstacles)
     _family._s.setDir({1, speed.y / _family._s.getVel()});
 }
 
-glm::vec2 Fish::obstacleForces(std::vector<Obstacle> const& obstacles)
+using ObstacleHandler = std::function<void(Obstacle const&)>;
+
+glm::vec2 Fish::obstacleForces(std::function<void(ObstacleHandler)> const& for_each_obstacle)
 {
     glm::vec2 oForce(0, 0);
-    for (const auto& obstacle : obstacles)
-    {
+
+    for_each_obstacle([&](const auto& obstacle) {
         if (glm::distance(_pos, obstacle.getPos()) < obstacle.getRadius() * 1.5)
         {
             oForce += glm::normalize(_pos - obstacle.getPos());
         }
-    }
+    });
 
     oForce *= 0.01;
 
@@ -146,8 +148,8 @@ glm::vec2 Fish::foodForces(std::vector<Food> const& food, int boidsLength) const
 
     for (const auto& f : food)
     {
-        fForce += glm::normalize(f.getPos() - _pos);
-        fForce *= 0.001f / glm::distance(f.getPos(), _pos);
+        fForce += glm::normalize(f.getPos() - _pos)
+                  * 0.001f / glm::distance(f.getPos(), _pos);
     }
 
     fForce /= boidsLength;
--- src/imgui.cpp
+++ src/imgui.cpp
@@ -4,12 +4,16 @@
 
 void imguiinit(p6::Context* ctx, Params& p, int& fishNb, bool& nbChanged, bool& sizeChanged)
 {
-    std::string text = "Hello";
+    //  std::string text = "Hello";
 
     (*ctx).imgui = [&]() {
         // Show a simple window
         ImGui::Begin("Boids sliders");
-        nbChanged   = ImGui::SliderInt("fish number", &fishNb, 1, 250);
+        nbChanged =
+            if (ImGui::SliderInt("fish number", &fishNb, 1, 250)) // USe this approach instead of storing booleans
+        {
+            boid.resize;
+        }
         sizeChanged = ImGui::SliderFloat("fish size", &p.fishSize, 0.02f, 0.08f);
         ImGui::SliderFloat("separation strength", &p.sepStr, 0.001f, 0.01f);
         ImGui::SliderFloat("alignment strength", &p.aliStr, 0.005f, 0.05f);
